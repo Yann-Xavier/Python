ad3c5784e63689f21358d3227dcb2b9378fc4e9c
👨‍💻 Yann Xavier <135256883+Yann-Xavier@users.noreply.github.com>
2024-03-12 09:19:36 -0300

    Update Calculo da Área.py
    Correção do código.

--- Calculo da Área.py
+++ Calculo da Área.py
@@ -1,111 +1,53 @@
-{
-  "nbformat": 4,
-  "nbformat_minor": 0,
-  "metadata": {
-    "colab": {
-      "provenance": [],
-      "authorship_tag": "ABX9TyPWJ55cjMxkGRSOPMzfKh7t",
-      "include_colab_link": true
-    },
-    "kernelspec": {
-      "name": "python3",
-      "display_name": "Python 3"
-    },
-    "language_info": {
-      "name": "python"
-    }
-  },
-  "cells": [
-    {
-      "cell_type": "markdown",
-      "metadata": {
-        "id": "view-in-github",
-        "colab_type": "text"
-      },
-      "source": [
-        "<a href=\"https://colab.research.google.com/github/Yann-Xavier/Python/blob/main/Calculo%20da%20%C3%81rea.ipynb\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
-      ]
-    },
-    {
-      "cell_type": "code",
-      "source": [
-        "import math\n",
-        "\n",
-        "def calcular_area_circulo(raio):\n",
-        "    return math.pi * raio ** 2\n",
-        "\n",
-        "def calcular_area_retangulo(base, altura):\n",
-        "    return base * altura\n",
-        "\n",
-        "def calcular_area_triangulo(base, altura):\n",
-        "    return (base * altura) / 2\n",
-        "\n",
-        "def main():\n",
-        "    while True:\n",
-        "        print(\"\\nCALCULADORA DE ÁREAS DE FORMAS GEOMÉTRICAS\")\n",
-        "        print(\"Escolha a forma geométrica para calcular a área:\")\n",
-        "        print(\"1. Círculo\")\n",
-        "        print(\"2. Retângulo\")\n",
-        "        print(\"3. Triângulo\")\n",
-        "        print(\"0. Sair\")\n",
-        "\n",
-        "        opcao = input(\"Digite o número correspondente à forma desejada: \")\n",
-        "\n",
-        "        if opcao == \"1\":\n",
-        "            try:\n",
-        "                raio = float(input(\"Digite o raio do círculo: \"))\n",
-        "                area = calcular_area_circulo(raio)\n",
-        "                print(\"A área do círculo é:\", area)\n",
-        "            except ValueError:\n",
-        "                print(\"Por favor, insira um valor numérico válido para o raio.\")\n",
-        "        elif opcao == \"2\":\n",
-        "            try:\n",
-        "                base = float(input(\"Digite a base do retângulo: \"))\n",
-        "                altura = float(input(\"Digite a altura do retângulo: \"))\n",
-        "                area = calcular_area_retangulo(base, altura)\n",
-        "                print(\"A área do retângulo é:\", area)\n",
-        "            except ValueError:\n",
-        "                print(\"Por favor, insira valores numéricos válidos para a base e altura.\")\n",
-        "        elif opcao == \"3\":\n",
-        "            try:\n",
-        "                base = float(input(\"Digite a base do triângulo: \"))\n",
-        "                altura = float(input(\"Digite a altura do triângulo: \"))\n",
-        "                area = calcular_area_triangulo(base, altura)\n",
-        "                print(\"A área do triângulo é:\", area)\n",
-        "            except ValueError:\n",
-        "                print(\"Por favor, insira valores numéricos válidos para a base e altura.\")\n",
-        "        elif opcao == \"0\":\n",
-        "            print(\"Obrigado por usar a calculadora de áreas. Até logo!\")\n",
-        "            break\n",
-        "        else:\n",
-        "            print(\"Opção inválida. Por favor, escolha uma opção válida.\")\n",
-        "\n",
-        "if __name__ == \"__main__\":\n",
-        "    main()\n"
-      ],
-      "metadata": {
-        "colab": {
-          "base_uri": "https://localhost:8080/"
-        },
-        "id": "7aERndcy4TPl",
-        "outputId": "4b7abaec-ae3c-4b16-86b9-4dc22f1ca414"
-      },
-      "execution_count": null,
-      "outputs": [
-        {
-          "output_type": "stream",
-          "name": "stdout",
-          "text": [
-            "\n",
-            "CALCULADORA DE ÁREAS DE FORMAS GEOMÉTRICAS\n",
-            "Escolha a forma geométrica para calcular a área:\n",
-            "1. Círculo\n",
-            "2. Retângulo\n",
-            "3. Triângulo\n",
-            "0. Sair\n"
-          ]
-        }
-      ]
-    }
-  ]
-}
+import math
+
+def calcular_area_circulo(raio):
+    return math.pi * raio ** 2
+
+def calcular_area_retangulo(base, altura):
+    return base * altura
+
+def calcular_area_triangulo(base, altura):
+    return (base * altura) / 2
+
+def main():
+    while True:
+        print("\nCALCULADORA DE ÁREAS DE FORMAS GEOMÉTRICAS")
+        print("Escolha a forma geométrica para calcular a área:")
+        print("1. Círculo")
+        print("2. Retângulo")
+        print("3. Triângulo")
+        print("0. Sair")
+
+        opcao = input("Digite o número correspondente à forma desejada: ")
+
+        if opcao == "1":
+            try:
+                raio = float(input("Digite o raio do círculo: "))
+                area = calcular_area_circulo(raio)
+                print("A área do círculo é:", area)
+            except ValueError:
+                print("Por favor, insira um valor numérico válido para o raio.")
+        elif opcao == "2":
+            try:
+                base = float(input("Digite a base do retângulo: "))
+                altura = float(input("Digite a altura do retângulo: "))
+                area = calcular_area_retangulo(base, altura)
+                print("A área do retângulo é:", area)
+            except ValueError:
+                print("Por favor, insira valores numéricos válidos para a base e altura.")
+        elif opcao == "3":
+            try:
+                base = float(input("Digite a base do triângulo: "))
+                altura = float(input("Digite a altura do triângulo: "))
+                area = calcular_area_triangulo(base, altura)
+                print("A área do triângulo é:", area)
+            except ValueError:
+                print("Por favor, insira valores numéricos válidos para a base e altura.")
+        elif opcao == "0":
+            print("Obrigado por usar a calculadora de áreas. Até logo!")
+            break
+        else:
+            print("Opção inválida. Por favor, escolha uma opção válida.")
+
+if __name__ == "__main__":
+    main()
